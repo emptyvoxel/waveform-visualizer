261fc7abe7e886bbf79d05fb379e157348e20403
emptyvoxel <89980949+emptyvoxel@users.noreply.github.com>
2021-10-03 23:07:28 -0300

    Display second audio channel

--- assets/js/index.js
+++ assets/js/index.js
@@ -40,27 +40,54 @@ function normalize (data) {
     return output;
 }
 
-function process (audioData) {
-    const channel = normalize(resample(audioData.getChannelData(0)));
-
-    // SVG > Canvas
+function createSVG (n) {
     const svg = document.createElementNS(URI, 'svg');
-    svg.setAttribute('class', 'svg');
+    svg.setAttribute('class', `svg${n}`);
     svg.setAttribute('height', '100px');
     svg.setAttribute('width', `${SAMPLES}px`);
 
+    return svg;
+}
+
+function createPath () {
     const path = document.createElementNS(URI, 'path');
     path.setAttribute('fill', '#0ff');
     path.setAttribute('stroke', '#0ff');
     path.setAttribute('stroke-width', '1px');
 
+    return path;
+}
+
+function drawPath (channel) {
     let d = 'M0,0';
     for (let i = 0; i < channel.length; i++) {
         d += ` L${i},${channel[i] * 100}`;
     }
     d += ' Z';
 
-    path.setAttribute('d', d);
-    svg.appendChild(path);
-    waveform.appendChild(svg);
+    return d;
+}
+
+function process (audioData) {
+    const channel1 = normalize(resample(audioData.getChannelData(0)));
+    const channel2 = normalize(resample(audioData.getChannelData(1)));
+
+    // SVG > Canvas
+    const svg1 = createSVG(1);
+    const svg2 = createSVG(2);
+
+    const path1 = createPath();
+    const path2 = createPath();
+
+    const d1 = drawPath(channel1);
+    const d2 = drawPath(channel2);
+
+    path1.setAttribute('d', d1);
+    path2.setAttribute('d', d2);
+    
+    svg1.appendChild(path1);
+    svg2.appendChild(path2);
+
+    waveform.appendChild(svg1);
+    waveform.appendChild(svg2);
 }
